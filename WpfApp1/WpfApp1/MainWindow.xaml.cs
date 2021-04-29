4cdfcf2e5572d3a20722aa0ab0abf4ce7361e986
oggep <54796870+oggep@users.noreply.github.com>
2021-04-29 11:00:17 +0200

    forre commit

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
 namespace WpfApp1
 {
     /// <summary>
@@ -20,12 +21,44 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        Rect playHitBox;
+        Rect groundHitBox;
+        Rect obstacleHitBox;
+
+        bool jumping;
+
+        int force = 20;
+
+        Random rnd = new Random();
+
+        bool gameover;
+
+        double spriteIndex = 0;
+
+        ImageBrush playerSprite = new ImageBrush();
+        ImageBrush backgroundSprite = new ImageBrush();
+        ImageBrush obstacleSprite = new ImageBrush();
+
+        int[] obstaclePosition = { 320, 310, 300 };
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void MyCanvas_KeyUp(object sender, KeyEventArgs e)
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+        private void StartGame()
+        {
+
+        }
+        private void RunSprite(double i)
         {
 
         }
